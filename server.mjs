1eb4b6e6c3032cc05bc0a2a7518a7e5e95cd77e6
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-24 21:54:08 +0100

    added featureRequest routes

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./src/controllers/userController.mjs";
+import featureRequestRoutes from "./src/controllers/featureRequestController.mjs";
 
 const app = express();
 const port = 3001;
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/feature-request", featureRequestRoutes);
 
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
