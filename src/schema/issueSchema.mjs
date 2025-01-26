c241ba13e009fdf61ba2c173b44c3e4f67875455
Bendik Kodehode <bendik08.kodehode@gmail.com>
2025-01-26 16:23:00 +0100

    added severity col

--- src/schema/issueSchema.mjs
+++ src/schema/issueSchema.mjs
@@ -3,6 +3,7 @@ import Joi from "joi";
 const IssueCreateSchema = Joi.object({
 	title: Joi.string().max(255).required(),
 	description: Joi.string().max(1000).optional(),
+	severity: Joi.string().valid("low", "medium", "high", "critical").required(),
 });
 
 export { IssueCreateSchema };
